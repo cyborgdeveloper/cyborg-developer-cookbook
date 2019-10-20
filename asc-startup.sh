079926ec66f0e9065c2866c74a5d50a8f5c3c0de
Rodrigo Moutinho <rcm1989@gmail.com>
2019-10-20 13:27:29 -0300

    refactor: adjust asc startup script

--- asc-startup.sh
+++ asc-startup.sh
@@ -1,36 +1,32 @@
 #!/bin/sh
-output_folder=book-output
-release_folder=book-release
+release_folder=dist
+log=asciidoctor.log
 
-# put the name of your ebook here
 book_name=cyborg-developer-cookbook
-
 book_name_asc=$book_name.asc
-logs=$output_folder/ebook.log
 
 version=1.0
 
 revdate=$(date '+%Y-%m-%d')
 params="--attribute revnumber=$version --attribute revdate=$revdate -D $release_folder"
 
-mkdir -p $output_folder
 mkdir -p $release_folder
 
-printf "\n--------------------------------------------------------\n" >> $logs
-printf "$(date '+%Y-%m-%d %H:%M:%S') ---------- Exporting book formats...\n" >> $logs
+printf "\n--------------------------------------------------------\n" >> $log
+printf "$(date '+%Y-%m-%d %H:%M:%S') ---------- Exporting book formats...\n" >> $log
 
 printf "Converting to HTML...\n"
-asciidoctor $params $book_name_asc >> $logs
+asciidoctor $params $book_name_asc >> $log
 printf " -- HTML output at $book_name.html\n"
 
 printf "Converting to EPub...\n"
-asciidoctor-epub3 $params $book_name_asc >> $logs
+asciidoctor-epub3 $params $book_name_asc >> $log
 printf " -- Epub output at $book_name.epub\n"
 
 printf "Converting to Mobi (kf8)...\n"
-asciidoctor-epub3 $params -a ebook-format=kf8 $book_name_asc >> $logs
+asciidoctor-epub3 $params -a ebook-format=kf8 $book_name_asc >> $log
 printf " -- Mobi output at $book_name.mobi\n"
 
 printf "Converting to PDF... (this one takes a while)\n"
-asciidoctor-pdf $params $book_name_asc 2>/dev/null >> $logs
+asciidoctor-pdf $params $book_name_asc 2>/dev/null >> $log
 printf " -- PDF  output at $book_name.pdf\n"
